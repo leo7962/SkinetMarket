777fee34b88acc399aca2904307ae917ba6da06f
leo7962 <ingenieroleonardo@outlook.com>
2020-06-30 23:15:55 -0500

    Agregando analizadores y protección de contraseñas

--- SkinetMarket/Startup.cs
+++ SkinetMarket/Startup.cs
@@ -28,6 +28,7 @@ namespace SkinetMarket
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
