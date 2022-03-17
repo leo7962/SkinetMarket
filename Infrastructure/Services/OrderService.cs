0c8e533cdf7376720f4c8c500fb29c87334cd38e
Leonardo Fabián Hernández Peña <IngenieroLeonardo@outlook.com>
2022-03-16 22:51:34 -0500

    cambio de usabilidad en la interfaz por Nulll

--- Infrastructure/Services/OrderService.cs
+++ Infrastructure/Services/OrderService.cs
@@ -1,73 +1,70 @@
-using Core.Interfaces;
-using Core.Models;
-using Core.Models.OrderAggregate;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Interfaces;
+using Core.Models;
+using Core.Models.OrderAggregate;
 
-namespace Infrastructure.Services
+namespace Infrastructure.Services;
+
+public class OrderService : IOrderService
 {
-    public class OrderService : IOrderService
+    private readonly IBasketRepository _basketRepo;
+    private readonly IGenericRepository<DeliveryMethod> _dmRepo;
+    private readonly IGenericRepository<Order> _orderRepo;
+    private readonly IGenericRepository<Product> _productRepo;
+
+    public OrderService(IGenericRepository<Order> orderRepo, IGenericRepository<DeliveryMethod> dmRepo,
+        IGenericRepository<Product> productRepo, IBasketRepository basketRepo)
     {
-        private readonly IBasketRepository _basketRepo;
-        private readonly IGenericRepository<DeliveryMethod> _dmRepo;
-        private readonly IGenericRepository<Order> _orderRepo;
-        private readonly IGenericRepository<Product> _productRepo;
+        _orderRepo = orderRepo;
+        _dmRepo = dmRepo;
+        _productRepo = productRepo;
+        _basketRepo = basketRepo;
+    }
 
-        public OrderService(IGenericRepository<Order> orderRepo, IGenericRepository<DeliveryMethod> dmRepo,
-            IGenericRepository<Product> productRepo, IBasketRepository basketRepo)
+    public async Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethodId, string basketId,
+        Address shippingAddress)
+    {
+        //Get Basket From the repo
+        var basket = await _basketRepo.GetBasketAsync(basketId);
+
+        //Get items from the product repo
+        var items = new List<OrderItem>();
+        foreach (var item in basket.Items)
         {
-            _orderRepo = orderRepo;
-            _dmRepo = dmRepo;
-            _productRepo = productRepo;
-            _basketRepo = basketRepo;
+            var productItem = await _productRepo.GetByIdAsync(item.Id);
+            var itemOrderded = new ProductItemOrder(productItem.Id, productItem.Name, productItem.PictureUrl);
+            var orderItem = new OrderItem(itemOrderded, productItem.Price, item.Quantity);
+            items.Add(orderItem);
         }
 
-        public async Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethodId, string basketId,
-            Address shippingAddress)
-        {
-            //Get Basket From the repo
-            var basket = await _basketRepo.GetBasketAsync(basketId);
+        //Get delivery method from repo
+        var deliveryMethod = await _dmRepo.GetByIdAsync(deliveryMethodId);
 
-            //Get items from the product repo
-            var items = new List<OrderItem>();
-            foreach (var item in basket.Items)
-            {
-                var productItem = await _productRepo.GetByIdAsync(item.Id);
-                var itemOrderded = new ProductItemOrder(productItem.Id, productItem.Name, productItem.PictureUrl);
-                var orderItem = new OrderItem(itemOrderded, productItem.Price, item.Quantity);
-                items.Add(orderItem);
-            }
+        //Calculate subtotal
+        var subtotal = items.Sum(item => item.Price * item.Quantity);
 
-            //Get delivery method from repo
-            var deliveryMethod = await _dmRepo.GetByIdAsync(deliveryMethodId);
+        //Create Order
+        var order = new Order(items, buyerEmail, shippingAddress, deliveryMethod, subtotal);
 
-            //Calculate subtotal 
-            var subtotal = items.Sum(item => item.Price * item.Quantity);
 
-            //Create Order
-            var order = new Order(items, buyerEmail, shippingAddress, deliveryMethod, subtotal);
+        //Return Order
+        return order;
+    }
 
-            //TODO: Save tod Db
+    public Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail)
+    {
+        return null;
+    }
 
-            //Return Order
-            return order;
-        }
+    public Task<Order> GetOrderByIdAsync(int id, string buyerEmail)
+    {
+        return null;
+    }
 
-        public async Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task<Order> GetOrderByIdAsync(int id, string buyerEmail)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
-        {
-            throw new NotImplementedException();
-        }
+    public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
+    {
+        return null;
     }
 }
